3d449a2e344e081b3268ec34e390363ce9648652
ivanglavatchi20 <ivanglavat@gmail.com>
2026-09-12 19:47:33 +0300

    add voobshe vse!!!

--- VspomnilAboutProject/Source.cpp
+++ VspomnilAboutProject/Source.cpp
@@ -20,20 +20,22 @@ public:
 		std::cout << Name << " is not a client anymore." << std::endl;
 	}
 		
-	float toDeposit()
+	float toDeposit(float Payment)
 	{
-		std::cout << "Write your payment" << std::endl;
-		float Payment; std::cin >> Payment;
 		Balance = this->Balance + Payment;
 		return Balance; 
 	}
 
-	float toWithdraw()
+	float toWithdraw(float WithdrawSum)
 	{
-		std::cout << "Write the sum you are willing to withdraw" << std::endl;
-		float WithdrawSum; std::cin >> WithdrawSum;
-		Balance = this->Balance - WithdrawSum;
-		return Balance;
+		if (WithdrawSum <= Balance) {
+			Balance = this->Balance - WithdrawSum;
+			return Balance;
+		}
+		else {
+			std::cout << "Not enough money on your account!" << std::endl;
+			return Balance;
+		}
 	}
 };
 
@@ -43,8 +45,19 @@ int main()
 	std::cout << "What is your account number?" << std::endl;
 	
 	short accNumber; std::cin >> accNumber;
-	
 
+	
+	//Bank Michael(002, "Michael", 0);
+	//Bank Jacob(003, "Jacob", 0);
+
+	float Payment;
+	
+	std::string Name;
+
+	std::cout << "What is your name?" << std::endl;
+	std::cin >> Name;
+
+	Bank Obj1(001, Name, 0);
 
 	std::cout << "Choose your operation" << std::endl;
 	std::cout << "1 - withdraw money" << std::endl;
@@ -53,21 +66,24 @@ int main()
 
 	if (operationOperator == 1) 
 	{	
-		
-		std::cout << "New balance is" << /*toWithdraw <<*/ std::endl;
+		std::cout << "Write your withdrawal amount" << std::endl;
+		float WithdrawSum; std::cin >> WithdrawSum;
+
+		std::cout << "New balance is " << Obj1.toWithdraw(WithdrawSum) << std::endl;
 	}
 	else if (operationOperator == 2) 
-	{
-		std::cout << "New balance is" << /*toDeposit  <<*/ std::endl;
+	{	
+		std::cout << "Write your deposit" << std::endl;
+
+		std::cin >> Payment;
+
+		std::cout << "New balance is " << Obj1.toDeposit(Payment) << std::endl;
 	}
 	else 
 	{
+		std::cout << "Ti che, debil? Vvedi normal'noe chislo!" << std::endl;
 	};
 	//Картофельный ахерел в край. Его надо забанить.
-
-	Bank John(001, "John", 0);
-	Bank Michael(002, "Michael", 0);
-	Bank Jacob(003, "Jacob", 0);
 
 	return 0;
 }
